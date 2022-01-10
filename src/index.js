230c8f57211b72c12f512ee811f92928020a8b76
Bricktech <bricktech2000@gmail.com>
2022-01-10 12:36:33 -0500

    temporary commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 const { exit } = require('process');
-const {
-  Client,
-  Intents,
-  MessageEmbed,
-  CommandInteractionOptionResolver,
-} = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
 if (process.argv.length != 3) {
   console.log('Usage: node . DISCORD_BOT_TOKEN');
@@ -30,14 +25,21 @@ client.on('messageCreate', async (msg) => {
 client.login(discordBotToken);
 
 const getReplyFromMessage = async (msg) => {
-  const isMentioned = msg.mentions.members.first()?.id == client.user.id;
+  // const isMentioned = msg.mentions.members.first()?.id == client.user.id;
   // if (isMentioned) return 'message registered.';
   // else return undefined;
 
   // https://stackoverflow.com/questions/432493/how-do-you-access-the-matched-groups-in-a-javascript-regular-expression
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
   const mentions = msg.content.matchAll(/@\`(.*?)\`/g);
-  for (mention of mentions) {
-    console.log(mention[1]);
+  for (const mention of mentions) {
+    const source = mention[1];
+    // https://javascript.info/new-function
+    const func = new Function(['parameter'], 'return ' + source);
+    console.log(source);
+    console.log(func('parameterValue'));
+    if (func('parameterValue')) {
+      return `message registered. early return: ${func('parameterValue')}`;
+    }
   }
 };
